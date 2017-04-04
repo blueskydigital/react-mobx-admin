a8279bb771e0409a95397d562a81273683b9fcc1
Vaclav Klecanda <vencax77@gmail.com>
2017-04-04 09:08:13 +0200

    view refactor

--- components/bstrap/view/list.js
+++ components/bstrap/view/list.js
@@ -3,85 +3,85 @@ import Datagrid from '../datagrid/datagrid'
 import Filters from '../datagrid/filters'
 import Pagination from '../datagrid/pagination'
 import DatagridActions from '../../common/datagrid/actions'
+import { observer } from 'mobx-react'
 
 
-export default class BStrapListView extends React.Component {
+const BStrapListView = ({
+  state, onAddClicked, fields, filters, listActions, batchActions, renderOuter
+}) => {
 
-  onSelectionChange(selection) {
+  function onSelectionChange(selection) {
     if(selection === 'all') {
-      this.props.state.selectAll(this.props.state.currentView)
+      state.selectAll(state.currentView)
     } else if(selection === []) {
-      this.props.state.updateSelection(this.props.state.currentView, [])
+      state.updateSelection(state.currentView, [])
     } else { // we have receive index of selected item
       // so toggle the selection of da index
-      this.props.state.toggleIndex(this.props.state.currentView, selection)
+      state.toggleIndex(state.currentView, selection)
     }
   }
 
-  render() {
-    const { state, onAddClicked } = this.props
+  function isSelected(idx) {
+    return state.currentView.selection.indexOf(idx) >= 0
+  }
 
-    function isSelected(idx) {
-      return state.currentView.selection.indexOf(idx) >= 0
-    }
+  const loading = (! state.currentView.items) || state.currentView.items.length === 0
 
-    const loading = (! state.currentView.items) || state.currentView.items.length === 0
+  if(loading) {
+    return <span className="is-loading">loading</span>
+  }
 
-    if(loading) {
-      return <span className="is-loading">loading</span>
-    }
+  const allSelected = state.currentView.selection.length === state.currentView.items.length
 
-    const allSelected = state.currentView.selection.length === state.currentView.items.length
+  const filtersRender = (filters && ! loading) ? (
+    <Filters.Controls state={state}
+      hideFilter={(filter)=>state.hideFilter(state.currentView, filter)} filters={filters} />
+  ) : null
+  const grid = (
+    <Datagrid items={state.currentView.items} attrs={state.currentView.attrs}
+      titles={state.currentView.headertitles} fields={fields}
+      rowId={(row)=>row[state.currentView.pkName]}
+      listActions={listActions}
+      onSort={(field, dir)=>state.updateSort(state.currentView, field, dir)} sortstate={state.currentView}
+      onRowSelection={onSelectionChange} isSelected={isSelected}
+      allSelected={allSelected} />
+  )
 
-    const filters = (this.filters && ! loading) ? (
-      <Filters.Controls state={state}
-        hideFilter={(filter)=>state.hideFilter(state.currentView, filter)} filters={this.filters} />
-    ) : null
-    const grid = (
-      <Datagrid items={state.currentView.items} attrs={state.currentView.attrs}
-        titles={state.currentView.headertitles} fields={this.fields}
-        rowId={(row)=>row[state.currentView.pkName]}
-        listActions={this.listActions ? this.listActions.bind(this) : undefined}
-        onSort={(field, dir)=>state.updateSort(state.currentView, field, dir)} sortstate={state.currentView}
-        onRowSelection={this.onSelectionChange.bind(this)} isSelected={isSelected}
-        allSelected={allSelected} />
-    )
-
-    const result = (
-      <div className="card">
-        <div className="card-block">
-          <div className="pull-right">
-            <Filters.Apply state={state} label={'apply filters'} apply={()=>state.applyFilters(state.currentView)} />
-            {this.batchActions && (<DatagridActions state={state} actions={this.batchActions} />)}
-            {this.filters && (
-              <Filters.Dropdown state={state} title="addfilter" filters={this.filters}
-                showFilter={(filter)=>state.showFilter(state.currentView, filter)} />
-            )}
-            <button type="button" className="btn btn-primary"
-              onClick={()=>onAddClicked(state)}>{state.currentView.addText || '+'}</button>
-          </div>
-          {state.currentView.title ? <h4 className="card-title">{state.currentView.title}</h4> : null}
+  const result = (
+    <div className="card">
+      <div className="card-block">
+        <div className="pull-right">
+          <Filters.Apply state={state} label={'apply filters'} apply={()=>state.applyFilters(state.currentView)} />
+          {batchActions && (<DatagridActions state={state} actions={batchActions} />)}
+          {filters && (
+            <Filters.Dropdown state={state} title="addfilter" filters={filters}
+              showFilter={(filter)=>state.showFilter(state.currentView, filter)} />
+          )}
+          <button type="button" className="btn btn-primary"
+            onClick={()=>onAddClicked(state)}>{state.currentView.addText || '+'}</button>
         </div>
-        { filters }
-        <div className="card-block">
-          { grid }
+        {state.currentView.title ? <h4 className="card-title">{state.currentView.title}</h4> : null}
+      </div>
+      { filtersRender }
+      <div className="card-block">
+        { grid }
+      </div>
+      <div className="card-block">
+        <div className="pull-right">
+          <Pagination.Pagination state={state} onChange={(page)=>state.updatePage(state.currentView, page)} />
         </div>
-        <div className="card-block">
-          <div className="pull-right">
-            <Pagination.Pagination state={state} onChange={(page)=>state.updatePage(state.currentView, page)} />
-          </div>
-          <div className="pull-left">
-            <Pagination.PageInfo info={state.currentView} />
-          </div>
+        <div className="pull-left">
+          <Pagination.PageInfo info={state.currentView} />
         </div>
       </div>
-    )
+    </div>
+  )
 
-    return this.renderOuter ? this.renderOuter(result) : result
-  }
-
-  static propTypes = {
-    state: React.PropTypes.object.isRequired
-  }
-
+  return renderOuter ? renderOuter(result) : result
 }
+
+BStrapListView.propTypes = {
+  state: React.PropTypes.object.isRequired,
+  renderOuter: React.PropTypes.func
+}
+export default observer(BStrapListView)
