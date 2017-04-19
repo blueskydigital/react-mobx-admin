b9208e2c709caebf699a19c6aea72d3b80f20b12
Vaclav Klecanda <vencax77@gmail.com>
2017-04-19 14:01:42 +0200

    fix

--- components/bstrap/field/bool.js
+++ components/bstrap/field/bool.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BStrapBoolField = ({record, attr}) => {
   const checked = Boolean(record[attr])
   return (
-    <input className="form-check-input" name={attr} type="checkbox" value={checked} disabled />
+    <input className="form-check-input" name={attr} type="checkbox" checked={checked} disabled />
   )
 }
 
