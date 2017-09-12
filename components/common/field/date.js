13c364ed36a2886fb41e3f2a451737ec1675cf0c
Adam Jun <adam@bluesky.pro>
2017-09-12 09:47:09 +0200

    y-m-d date format function

--- components/common/field/date.js
+++ components/common/field/date.js
@@ -2,12 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const DateField = ({ attr, val, Component }) => {
+  function formateDate ({ date = new Date(), separator = '-', order = ['year', 'month', 'day'] }) {
+    let obj = {
+      'year': date.getFullYear(),
+      'month': (date.getMonth() + 1 < 10) ? ('0' + (date.getMonth() + 1)) : (date.getMonth() + 1),
+      'day': (date.getDate() < 10) ? ('0' + date.getDate()) : date.getDate()
+    }
+    return `${obj[order[0]]}${separator}${obj[order[1]]}${separator}${obj[order[2]]}`
+  }
+
   if (!val) {
     return null
   }
 
   const d = val instanceof Date ? d : new Date(val)
-  const text = d.toLocaleDateString()
+  const text = formateDate({date: d})
   return Component ? <Component attr={attr} text={text} /> : <span>{text}</span>
 }
 
