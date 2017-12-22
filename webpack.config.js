cce6d0e00c668a7380970736c4e4be83e270a858
vencax <vencax@centrum.cz>
2017-12-22 13:55:53 +0100

    babelrc remove, libs externalized, dependencies updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,46 @@
 var debug = process.env.NODE_ENV !== 'production'
-var webpack = require('webpack')
 var path = require('path')
+
+var babelOptions = {
+  'presets': ['react', 'stage-0', ['es2015', {'modules': false}]],
+  'plugins': [
+    'transform-object-rest-spread',
+    'transform-decorators-legacy',
+    'transform-class-properties'
+  ]
+}
 
 module.exports = {
   devtool: debug ? 'inline-sourcemap' : null,
   entry: './examples/blog/js/main.js',
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        // NOTE: you need modify exclude regexp when used in separate project
-        // to allow babel to transpile!!!
-        // E.g. /node_modules(?!\/react-mobx-admin)/
-        exclude: /node_modules(?!\/mobx-router)/,
-        loader: 'babel-loader'
-      }
-    ]
+    loaders: [{
+      test: /\.js$/,
+      // NOTE: you need modify exclude regexp when used in separate project
+      // to allow babel to transpile!!!
+      // E.g. /node_modules(?!\/react-mobx-admin)/
+      exclude: /node_modules(?!\/mobx-router)/,
+      loader: 'babel-loader',
+      options: babelOptions
+    }]
   },
   output: {
     path: path.join(__dirname, 'examples/blog'),
     filename: 'main.min.js'
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
   // NOTE: needed coz' import like from a separate project are used in example
   // e.g.: import DataRequester from 'react-mobx-admin/services/requester'
   resolve: {
     alias: {
       'react-mobx-admin': __dirname,
-      'mobx-router': path.join(__dirname, 'node_modules', 'mobx-router'),
+      'mobx-router': path.join(__dirname, 'node_modules', 'mobx-router')
     }
+  },
+  externals: {
+    'axios': 'axios',
+    'mobx': 'mobx',
+    'mobx-react': 'mobxReact',
+    'react': 'React',
+    'react-dom': 'ReactDOM'
   }
 }
