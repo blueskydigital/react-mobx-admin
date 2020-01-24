b4df36dd1a16a653474e7523ebb3e285f7be852a
Dušan Marsa <dusanmarsa99@gmail.com>
2020-01-24 14:27:13 +0100

    Update actions.js
    Added overlay for batchActions.
    - simulate disabled state with styles and disable dropdown opening with "open: false", also add  "overlay" props which will encapsulate component with overlay (have to be <Button>).

--- components/common/datagrid/actions.js
+++ components/common/datagrid/actions.js
@@ -2,8 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 
-const DatagridActions = ({actions, state}) => {
-  return (state.selection.length > 0) ? actions(state) : React.cloneElement(actions(state), { disabled: true })
+const DatagridActions = ({ actions, state, overlay }) => {
+  let actionsElement = (state.selection.length > 0)
+    ? actions(state)
+    : React.cloneElement(actions(state), {
+      open: false, // closed dropdown if items not selected
+      style: { opacity: .65, cursor: 'not-allowed' }
+    })
+  return overlay && typeof overlay === 'function' ? overlay(actionsElement) : actionsElement
 }
 
 DatagridActions.propTypes = {
