f2fff63f465ddb9d8bccf783d3dd7a5c223fe07b
Adam Jun <adam@bluesky.pro>
2020-08-19 10:20:18 +0200

    If valid_from and valid_to dates are set together, use the success/danger class for the warning about availability

--- components/common/field/date.js
+++ components/common/field/date.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 
-const DateField = ({ attr, val, Component }) => {
+const DateField = ({ attr, val, valN, Component }) => {
   function formateDate ({ date = new Date(), separator = '-', order = ['year', 'month', 'day'] }) {
-    let obj = {
-      'year': date.getUTCFullYear(),
-      'month': (date.getUTCMonth() + 1 < 10) ? ('0' + (date.getUTCMonth() + 1)) : (date.getUTCMonth() + 1),
-      'day': (date.getUTCDate() < 10) ? ('0' + date.getUTCDate()) : date.getUTCDate()
+    const obj = {
+      year: date.getUTCFullYear(),
+      month: (date.getUTCMonth() + 1 < 10) ? ('0' + (date.getUTCMonth() + 1)) : (date.getUTCMonth() + 1),
+      day: (date.getUTCDate() < 10) ? ('0' + date.getUTCDate()) : date.getUTCDate()
     }
     return `${obj[order[0]]}${separator}${obj[order[1]]}${separator}${obj[order[2]]}`
   }
@@ -16,8 +17,31 @@ const DateField = ({ attr, val, Component }) => {
   }
 
   const d = val instanceof Date ? d : new Date(val)
-  const text = formateDate({date: d})
-  return Component ? <Component attr={attr} text={text} /> : <span className='one-line-value'>{text}</span>
+  const text = formateDate({ date: d })
+  const validDate = moment(text).isValid() && moment(text)
+  const validDateN = valN && moment(valN).isValid() && moment(valN)
+  let addClass = ''
+
+  switch (attr) {
+  case 'valid_from':
+    addClass = validDateN
+      ? (moment().isSameOrAfter(validDate, 'day') && moment().isSameOrBefore(validDateN, 'day')
+        ? 'text-success'
+        : 'text-danger')
+      : ''
+    break
+  case 'valid_to':
+    addClass = validDateN
+      ? (moment().isSameOrBefore(validDate, 'day') && moment().isSameOrAfter(validDateN, 'day')
+        ? 'text-success'
+        : 'text-danger')
+      : ''
+    break
+  }
+
+  return Component
+    ? <Component attr={attr} text={text} />
+    : <span className={'one-line-value ' + addClass}>{text}</span>
 }
 
 DateField.propTypes = {
