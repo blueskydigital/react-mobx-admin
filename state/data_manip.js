609370676fb9f0163056ab812f2e23393a5d2260
Adam Jun <adam@bluesky.pro>
2020-10-08 14:38:09 +0200

    beforeSave - set updated_at because of save same date to the origRecord

--- state/data_manip.js
+++ state/data_manip.js
@@ -1,8 +1,7 @@
-import {observable, computed, action, toJS} from 'mobx'
+import { observable, computed, action, toJS } from 'mobx'
 import deepEqual from 'deep-equal'
 
 export default class DataManipState {
-
   @observable entityname = null
   @observable origRecordId = null
   @observable record = new Map()
@@ -12,7 +11,7 @@ export default class DataManipState {
 
   pkName = 'id'
 
-  constructor(entityname, id, saveEntry) {
+  constructor (entityname, id, saveEntry) {
     if (this.prepareNew === undefined) {
       throw new Error('implement prepareNew method!')
     }
@@ -21,13 +20,13 @@ export default class DataManipState {
     this.origRecordId = id
   }
 
-  _onPrepared() {
+  _onPrepared () {
     this.origRecord = toJS(this.record)
     this._runValidators()
     this.state = 'ready'
   }
 
-  init() {
+  init () {
     if (this.origRecordId === null) {
       const promise = this.prepareNew()
       if (!promise) {
@@ -38,25 +37,24 @@ export default class DataManipState {
     }
   }
 
-  @action onLoaded(record) {
-    this.origRecord = JSON.parse(JSON.stringify(record))  // deep clone :)
+  @action onLoaded (record) {
+    this.origRecord = JSON.parse(JSON.stringify(record)) // deep clone :)
     this.record.merge(record)
     this._runValidators()
     this.state = 'ready'
   }
 
-  _runValidators(opts) {
-    for (let fieldName in this.validators) {
+  _runValidators (opts) {
+    for (const fieldName in this.validators) {
       const value = (fieldName === '_global') ? this.record : this.record.get(fieldName)
       this._validateField(fieldName, value, opts)
     }
   }
 
-  _validateField(fieldName, value, opts) {
+  _validateField (fieldName, value, opts) {
     if (this.validators && this.validators[fieldName]) {
       const validatorFn = this.validators[fieldName]
-
-      function runValidatorsArray (validatorArray) {
+      const runValidatorsArray = (validatorArray) => {
         const errors = []
         validatorArray.map(validatorFn => {
           const error = validatorFn.bind(this)(value, this.errors, opts)
@@ -69,7 +67,7 @@ export default class DataManipState {
       const error = validatorFn.call === undefined
         ? runValidatorsArray.bind(this)(validatorFn)
         : validatorFn.bind(this)(value, this.errors, opts)
-      if(error === undefined && this.errors.has(fieldName)) {
+      if (error === undefined && this.errors.has(fieldName)) {
         this.errors.delete(fieldName)
       } else if (error !== undefined) {
         this.errors.set(fieldName, error)
@@ -77,37 +75,40 @@ export default class DataManipState {
     }
   }
 
-  runGlobalValidator(opts) {
+  runGlobalValidator (opts) {
     this._validateField('_global', this.record, opts)
   }
 
-  @computed get isEntityChanged() {
+  @computed get isEntityChanged () {
     const record = toJS(this.record)
-    return ! deepEqual(this.origRecord, record, {strict: true})
+    return !deepEqual(this.origRecord, record, { strict: true })
   }
 
-  @action beforeSave(record) {
+  @action beforeSave (record) {
+    // set updated_at because of save same date to the origRecord
+    record.updated_at = new Date()
+
     this.origRecord = JSON.parse(JSON.stringify(record))
     this.record.merge(this.origRecord)
     this._runValidators()
   }
 
-  @action save() {
+  @action save () {
     this.state = 'saving'
     return Promise.resolve(this.beforeSave.bind(this)).then(() => {
-      if(this.errors.size) {
+      if (this.errors.size) {
         this.state = 'ready'
         return this.onError('Validation errors') // show validation error message
       }
       return this.saveEntry(this.entityname, this.record, this.origRecordId)
-      .then(this.onSaved.bind(this))
-      .catch(() => this.state = 'ready') // this.onError.bind(this) did not work properly
+        .then(this.onSaved.bind(this))
+        .catch(() => this.state = 'ready') // this.onError.bind(this) did not work properly
       // error is handled from saveEntry error handler
       // in catch we just need to edit state so that page does not hang on "loading" state
     })
   }
 
-  @action onSaved(saved) {
+  @action onSaved (saved) {
     this.origRecord = JSON.parse(JSON.stringify(saved)) // update origRecord coz saved
     this.record.clear()
     this.record.merge(saved)
@@ -121,14 +122,14 @@ export default class DataManipState {
     return this.record
   }
 
-  @action onError(err) {
+  @action onError (err) {
     this.state = 'ready'
     this.saveErrors = [err]
   }
 
   // called on each update of edit form. Validation performed if got some validators
   @action
-  updateData(fieldName, value) {
+  updateData (fieldName, value) {
     value = value === '' ? null : value
     this.record.set(fieldName, value)
     this._validateField(fieldName, value)
@@ -137,5 +138,4 @@ export default class DataManipState {
     this.onFieldChange && this.onFieldChange[fieldName] &&
       this.onFieldChange[fieldName](value)
   }
-
 }
